5160bb9acb2b6fdb9fb9ed3c2ede747b931f35f3
Jackie <jackie319@vip.qq.com>
2016-11-24 15:11:40 +0800

    cache

--- JK.Framework.Data/RegisterAutofacForJK.cs
+++ JK.Framework.Data/RegisterAutofacForJK.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autofac;
+using JK.Framework.Core.Caching;
 using JK.Framework.Core.Data;
 
 namespace JK.Framework.Data
@@ -17,6 +18,7 @@ namespace JK.Framework.Data
             //InstancePerDependency对每一个依赖或每一次调用创建一个新的唯一的实例
             //InstancePerLifetimeScope在一个生命周期域中，每一个依赖或调用创建一个单一的共享的实例，且每一个不同的生命周期域，实例是唯一的，不共享的。
             builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterType<MemoryCacheManager>().As<ICacheManager>().SingleInstance();
         }
     }
 

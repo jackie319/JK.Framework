b9bb29cad0b99c3b851003cdbb3ee97af8755a12
Jackie <jackie319@vip.qq.com>
2016-11-29 14:56:11 +0800

    plugin

---
+++ JK.Framework.Core/Plugins/IPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.Core.Plugins
+{
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Gets or sets the plugin descriptor
+        /// </summary>
+        PluginDescriptor PluginDescriptor { get; set; }
+
+        /// <summary>
+        /// Install plugin
+        /// </summary>
+        void Install();
+
+        /// <summary>
+        /// Uninstall plugin
+        /// </summary>
+        void Uninstall();
+    }
+}
---
+++ JK.Framework.Core/Plugins/PluginDescriptor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.Core.Plugins
+{
+    public class PluginDescriptor
+    {
+        public virtual string PluginFileName { get; set; }
+
+        /// <summary>
+        /// Plugin type
+        /// </summary>
+        public virtual Type PluginType { get; set; }
+
+        /// <summary>
+        /// The assembly that has been shadow copied that is active in the application
+        /// </summary>
+        public virtual Assembly ReferencedAssembly { get; internal set; }
+
+        /// <summary>
+        /// The original assembly file that a shadow copy was made from it
+        /// </summary>
+        public virtual FileInfo OriginalAssemblyFile { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the plugin group
+        /// </summary>
+        public virtual string Group { get; set; }
+
+        /// <summary>
+        /// Gets or sets the friendly name
+        /// </summary>
+        public virtual string FriendlyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the system name
+        /// </summary>
+        public virtual string SystemName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version
+        /// </summary>
+        public virtual string Version { get; set; }
+
+        /// <summary>
+        /// Gets or sets the supported versions of nopCommerce
+        /// </summary>
+        public virtual IList<string> SupportedVersions { get; set; }
+
+        /// <summary>
+        /// Gets or sets the author
+        /// </summary>
+        public virtual string Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display order
+        /// </summary>
+        public virtual int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of store identifiers in which this plugin is available. If empty, then this plugin is available in all stores
+        /// </summary>
+        public virtual IList<int> LimitedToStores { get; set; }
+
+    }
+}

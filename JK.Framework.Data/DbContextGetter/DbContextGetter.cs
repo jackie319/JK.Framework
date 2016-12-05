cb38a61b2c8cc5e5b382e25904ba0bc8e774db6b
Jackie <jackie319@vip.qq.com>
2016-12-05 15:15:44 +0800

    dbgetter

---
+++ JK.Framework.Data/DbContextGetter/DbContextGetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Integration.Mvc;
+
+namespace JK.Framework.Data
+{
+    public class DbContextGetter:IDbContextGetter
+    {
+        public T GetByName<T>(string name)
+        {
+            return AutofacDependencyResolver.Current.RequestLifetimeScope.ResolveNamed<T>(name);
+        }
+    }
+}
---
+++ JK.Framework.Data/DbContextGetter/IDbContextGetter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.Data
+{
+    public interface IDbContextGetter
+    {
+        T GetByName<T>(string name);
+    }
+
+}

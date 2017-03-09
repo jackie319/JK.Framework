05c4f14f2dd8aef85b8e49948547f48fbabc135f
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-03-09 14:34:54 +0800

    11

--- JK.Framework.Web/Model/JsonResultHelper.cs
+++ JK.Framework.Web/Model/JsonResultHelper.cs
@@ -9,24 +9,29 @@ namespace JK.Framework.Web.Model
 {
    public static class JsonResultHelper
     {
-       public static JsonResult JsonOk(this Controller left,bool success=true,string errorMsg="",string url="",object returnData=null)
+       public static JsonResult JsonOk(this Controller left, int total, bool success=true,string errorMsg="",string url="",object returnData=null)
        {
-           return new ResultModel(success,errorMsg,url,returnData).ToJsonResultModel();
+           return new ResultModel(success,errorMsg,total,url,returnData).ToJsonResultModel();
        }
 
-        public static JsonResult JsonOk(this Controller left,object returnData)
+        public static JsonResult JsonOk(this Controller left, int total, object returnData)
         {
-            return new ResultModel(true, "", "",returnData).ToJsonResultModel();
+            return new ResultModel(true, "",total, "",returnData).ToJsonResultModel();
         }
 
-        public static JsonResult JsonOk(bool success = true, string errorMsg = "",string url="", object returnData = null)
+        public static JsonResult JsonOk(this Controller left,  object returnData)
         {
-            return new ResultModel(success, errorMsg, url,returnData).ToJsonResultModel();
+            return new ResultModel(true, "", 1, "", returnData).ToJsonResultModel();
+        }
+
+        public static JsonResult JsonOk(int total, bool success = true, string errorMsg = "",string url="", object returnData = null)
+        {
+            return new ResultModel(success, errorMsg, total,url,returnData).ToJsonResultModel();
         }
 
         public static JsonResult JsonOk(object returnData)
         {
-            return new ResultModel(true,"", "",returnData).ToJsonResultModel();
+            return new ResultModel(true,"", 1,"",returnData).ToJsonResultModel();
         }
     }
 }
--- JK.Framework.Web/Model/ResultModel.cs
+++ JK.Framework.Web/Model/ResultModel.cs
@@ -13,12 +13,14 @@ namespace JK.Framework.Web.Model
         public virtual string ErrorUrl { get; set; }
         public virtual string ErrorMsg { set; get; }
 
+        public virtual int Total { set; get; }
         public virtual Object Data { set; get; }
-        public ResultModel(bool success, string erroMsg, string errorUrl = "", Object data = null)
+        public ResultModel(bool success, string erroMsg, int total, string errorUrl = "", Object data = null)
         {
             Success = success;
             ErrorMsg = erroMsg;
             ErrorUrl = erroMsg;
+            Total = total;
             Data = data;
         }
         internal JsonResultModel ToJsonResultModel()

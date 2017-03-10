1ea39ac3a3c3579b76ba2d352d0e4f00fad95db1
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-03-10 10:37:43 +0800

    querymodel

---
+++ JK.Framework.Web/Model/QueryBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.Web.Model
+{
+    public class QueryBase
+    {
+      
+        private int skip;
+        private int take = 20;
+
+        public int Skip
+        {
+            get {
+                if (skip > 0)
+                {
+                    return skip;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set { skip = value; }
+        }
+
+        public int Take
+        {
+            get
+            {
+                if (take < 0 || take > 1000)
+                {
+                    return 20;
+                }
+                else
+                {
+                    return take;
+                }
+            }
+            set { take = value; }
+        }
+
+
+    }
+}

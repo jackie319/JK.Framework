f151862130e23c2c67a20c663f984d969f5c2814
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-03-28 17:44:24 +0800

    IDependencyRegistrar

---
+++ JK.Framework.Core/Infrastructure/DependencyManagement/IDependencyRegistrar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace JK.Framework.Core.Infrastructure.DependencyManagement
+{
+    /// <summary>
+    /// Dependency registrar interface
+    /// </summary>
+    public interface IDependencyRegistrar
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="builder">Container builder</param>
+        void Register(ContainerBuilder builder);
+
+        /// <summary>
+        /// Order of this dependency registrar implementation
+        /// </summary>
+        int Order { get; }
+    }
+}

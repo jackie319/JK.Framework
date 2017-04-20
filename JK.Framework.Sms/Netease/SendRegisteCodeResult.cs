f59a63ead553b175042c989b6d3972d671c38aba
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-04-20 11:00:13 +0800

    sms ok

---
+++ JK.Framework.Sms/Netease/SendRegisteCodeResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.Sms.Netease
+{
+    public class SendRegisteCodeResult
+    {
+        public string code { get; set; }
+
+        public string msg { get; set; }
+
+        public string obj { get; set; }
+    }
+}
---
+++ JK.Framework.Sms/Netease/SmsCode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using JK.Framework.Extensions;
+using Newtonsoft.Json;
+
+namespace JK.Framework.Sms.Netease
+{
+    /// <summary>
+    /// 网易云短信
+    /// </summary>
+    public class SmsCode
+    {
+        public string Appkey { set; get; }
+        public string AppSecret { set; get; }
+        public string RegisteCodeTemplateid { set; get; }
+        public string RegisteUrl { set; get; }
+
+        public SmsCode(string appkey,string appSecret,string registeCodeTemplateid,string registeUrl)
+        {
+            Appkey = appkey;
+            AppSecret = appSecret;
+            RegisteCodeTemplateid = registeCodeTemplateid;
+            RegisteUrl = registeUrl;
+        }
+
+        public SendRegisteCodeResult SendRegisteCode(string phone)
+        {
+            using (var webClient = new WebClient { Encoding = Encoding.UTF8 })
+            {
+                string nonce = Guid.NewGuid().ToString("N");
+                DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0);
+                DateTime now = DateTime.Now;
+                string curTime = (now - dt).TotalSeconds.ToString();
+                var str = AppSecret + nonce + curTime;
+                var sha1 = str.SHA1_Encrypt();
+                webClient.Headers.Add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+                webClient.Headers.Add("AppKey", Appkey);
+                webClient.Headers.Add("Nonce", nonce);
+                webClient.Headers.Add("CurTime", curTime);
+                webClient.Headers.Add("CheckSum", sha1);
+                string url = RegisteUrl;
+                string data = "mobile=" + phone + "&templateid =" + RegisteCodeTemplateid + "&codeLen=6";
+                var result = webClient.UploadString(url, "POST", data);
+                SendRegisteCodeResult model = JsonConvert.DeserializeObject<SendRegisteCodeResult>(result);
+                return model;
+            }
+        }
+    }
+}

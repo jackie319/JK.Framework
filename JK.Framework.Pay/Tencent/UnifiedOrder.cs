f00fab0797c519b726941fa87e656b2128235dff
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-04-21 11:36:55 +0800

    11

---
+++ JK.Framework.Pay/Tencent/UnifiedOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Senparc.Weixin;
+using Senparc.Weixin.MP.TenPayLibV3;
+
+namespace JK.Framework.Pay.Tencent
+{
+    public static class UnifiedOrder
+    {
+        public static UnifiedorderResult Pay(TenPayV3UnifiedorderRequestData dataInfo, int timeOut = Config.TIME_OUT)
+        {
+            return TenPayV3.Unifiedorder(dataInfo, timeOut);
+        }
+    }
+}

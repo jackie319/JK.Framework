f72340e01abea9745abc621ff26adda7f1c1db69
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-06-08 17:37:33 +0800

    api autofac

--- JK.Framework.Data/RegisterAutofacForJK.cs
+++ JK.Framework.Data/RegisterAutofacForJK.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using JK.Framework.Core.Caching;
 using JK.Framework.Core.Data;
 
@@ -40,6 +42,30 @@ namespace JK.Framework.Data
 
             // then
             _container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(_container));
+
+        }
+        public static void RegisterApi(string connectionStr, RegisterAutofacDelegate registerAutofacDelegate)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            //builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces();
+
+            HttpConfiguration config = GlobalConfiguration.Configuration;
+
+            builder.Register<IDbContext>(c => new JKObjectContext(connectionStr)).InstancePerLifetimeScope();
+            //InstancePerDependency对每一个依赖或每一次调用创建一个新的唯一的实例
+            //InstancePerLifetimeScope在一个生命周期域中，每一个依赖或调用创建一个单一的共享的实例，且每一个不同的生命周期域，实例是唯一的，不共享的。
+            builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterType<MemoryCacheManager>().As<ICacheManager>().SingleInstance();
+            registerAutofacDelegate(builder);
+            //TODO:也可通过反射找到IDependencyRegistrar的实现类并调用方法
+
+            // then
+            _container = builder.Build();
+
+            //注册api容器需要使用HttpConfiguration对象
+             config.DependencyResolver = new AutofacWebApiDependencyResolver(_container);
+
             DependencyResolver.SetResolver(new AutofacDependencyResolver(_container));
 
         }

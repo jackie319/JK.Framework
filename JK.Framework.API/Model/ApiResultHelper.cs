ef274ff100cbbffe1d07b41cca6039a0cd1e2f8a
Jackie <Jackie@DESKTOP-D1DUJ56>
2017-06-12 17:13:57 +0800

    api resulthelper

---
+++ JK.Framework.API/Model/ApiResultHelper.cs
@@ -0,0 +1,37 @@
+﻿using JK.Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace JK.Framework.API.Model
+{
+    public static class ApiResultHelper
+    {
+        //public static ApiResultModel Result(this ApiController left, bool success = true, string errorMsg = "", int total = 0, string url = "", object returnData = null, JKExceptionType exceptionType = JKExceptionType.Common, string redirectUrl = "")
+        //{
+        //    return new ApiResultModel(success, errorMsg, total, url, exceptionType, redirectUrl, returnData);
+        //}
+
+        public static ApiResultModel ResultListModel(this ApiController left, int total, object returnData, JKExceptionType exceptionType = JKExceptionType.Common, string redirectUrl = "")
+        {
+            return new ApiResultModel(true, "", total, "", exceptionType, redirectUrl, returnData);
+        }
+        public static ApiResultModel ResultModel(this ApiController left, object returnData, JKExceptionType exceptionType = JKExceptionType.Common, string redirectUrl = "")
+        {
+            return new ApiResultModel(true, "", 1, "", exceptionType, redirectUrl, returnData);
+        }
+
+        public static ApiResultModel ResultSuccess(this ApiController left, JKExceptionType exceptionType = JKExceptionType.Common, string redirectUrl = "")
+        { 
+            return new ApiResultModel(true, "", 0, "", exceptionType, redirectUrl, null);
+        }
+
+        public static ApiResultModel ResultError(this ApiController left, string errorMsg, string errorUrl = "", JKExceptionType exceptionType = JKExceptionType.Common, string redirectUrl = "")
+        {
+            return new ApiResultModel(false, errorMsg, 0, errorUrl, exceptionType, redirectUrl, null);
+        }
+    }
+}
---
+++ JK.Framework.API/Model/ApiResultModel.cs
@@ -0,0 +1,57 @@
+﻿using JK.Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.API.Model
+{
+    /// <summary>
+    /// 此类同JK.Framework.Web.Model 类似，应该再降一级
+    /// </summary>
+    public class ApiResultModel
+    {
+        /// <summary>
+        /// 是否成功
+        /// </summary>
+        public virtual bool Success { set; get; }
+
+        /// <summary>
+        /// 出错地址
+        /// </summary>
+        public virtual string ErrorUrl { get; set; }
+        /// <summary>
+        /// 错误消息
+        /// </summary>
+        public virtual string ErrorMsg { set; get; }
+
+        /// <summary>
+        /// 异常类型
+        /// </summary>
+        public virtual JKExceptionType ExceptionType { set; get; }
+        /// <summary>
+        /// 跳转地址
+        /// </summary>
+        public string RedirectUrl { set; get; }
+        /// <summary>
+        /// 条目总数
+        /// </summary>
+        public virtual int Total { set; get; }
+        /// <summary>
+        /// 返回的数据
+        /// </summary>
+        public virtual Object Data { set; get; }
+
+        public ApiResultModel(bool success, string erroMsg, int total, string errorUrl = "", JKExceptionType exceptionType = JKExceptionType.Common, string redirectUrl = "", Object data = null)
+        {
+            Success = success;
+            ErrorMsg = erroMsg;
+            ErrorUrl = errorUrl;
+            Total = total;
+            ExceptionType = exceptionType;
+            RedirectUrl = redirectUrl;
+            Data = data;
+        }
+    }
+}

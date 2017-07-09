79d26b7fa87966a34dc5e9dead256d7e4488f428
jackie319 <jackie319@vip.qq.com>
2017-07-09 12:18:48 +0800

    logistics

---
+++ JK.Framework.Logistics/kdniao/SearchResult.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.Logistics.kdniao
+{
+    public class SearchResult
+    {
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public string EBusinessID { get; set; }
+        /// <summary>
+        /// 订单编号
+        /// </summary>
+        public string OrderCode { get; set; }
+        /// <summary>
+        /// 快递公司编码
+        /// </summary>
+        public string ShipperCode { get; set; }
+        /// <summary>
+        /// 物流运单号
+        /// </summary>
+        public string LogisticCode { get; set; }
+        /// <summary>
+        /// 成功与否
+        /// </summary>
+        public Boolean Success { get; set; }
+        /// <summary>
+        /// 失败原因
+        /// </summary>
+        public string Reason { get; set; }
+
+        /// <summary>
+        /// 流状态：2-在途中,3-签收,4-问题件
+        /// </summary>
+        public string State { get; set; }
+
+        public IList<Trace> Traces { get; set; }
+    }
+
+    public class Trace
+    {
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public string AcceptTime { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string AcceptStation { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}

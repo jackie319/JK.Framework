fd52723a914e5e616e9b023e27bf103aae315660
jackie319 <jackie319@vip.qq.com>
2017-08-22 10:06:05 +0800

    Aes

---
+++ JK.Framework.Extensions/Encryption/Base64.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.Framework.Extensions.Encryption
+{
+    public static class Base64
+    {
+        /// <summary>
+        /// 编码
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToBase64Str(this string value)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(value);
+            string result = Convert.ToBase64String(bytes);
+            return result;
+        }
+
+        /// <summary>
+        /// 解码
+        /// </summary>
+        /// <param name="base64Str"></param>
+        /// <returns></returns>
+        public static string FromBase64Str(this string value)
+        {
+            byte[] outputb = Convert.FromBase64String(value);
+            string result = Encoding.Default.GetString(outputb);
+            return result;
+        }
+    }
+}
--- JK.Framework.Test/UnitTest1.cs
+++ JK.Framework.Test/UnitTest1.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Text;
 using JK.Framework.Extensions.QrCode;
 using JK.Framework.Extensions.Zip;
+using JK.Framework.Extensions.Encryption;
+using JK.Framework.Extensions;
 
 namespace JK.Framework.Test
 {
@@ -65,6 +67,16 @@ namespace JK.Framework.Test
         {
             ZipHelper.ZipFile("D:\\erweima/", "");
             Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestDes()
+        {
+            string reqInfo = "yArzticOpSKuSEnHAw+qy78yYB846QSlwA7h92M24BzvegbMHObUPYIdBrUtIOLFG0iX3oKee0UQ+/Q9EV6UJ68EDbKpKBBR0SIxNnJtVQ9itUgbnscr9I3x6lTFED31BUBKo6Z8EQX9IxhapdJY8yumzFzv3QOub//vclrqNxP78Dt5fN1LKYjSbFaVv78/WhSVKnrVBx6O9YY7PKWCabMXT6cwf6Oj2vK78wEXJuHh88WVUB8K6R9XIC59Ay6KoIq/nqoY3Lc7fXPyrcD2ns2WiN0JPpuBlSJrchQo3YAjJg+dO6XDdD6naObHOWtXBeQ3ifjvmB1YHN/xRs4hCDCvTyQGSupL7th9wZciSHDP9+rbQMBZYGd5Q7nF3gkXT6XROaKxh8H1eKwSQAOrj9GWSxl0YXE3lHCQd4JcH3Tpe5Ms0w0bhE303fCH/mT2t6ODj2e4hNwU7uo9VYjxCCA3RHfzrZ91nuyoFwl1QIyNOGayKjk+rWF5wYlrfCwUn4QwbC6v6DDuQpbUmFQQYKp+zpCgItxFBsHk1rnNUW2l2RMS4wqH15WqruJfXR/mqOu7oLDgWVbikbp/dnPD7Mbcd5WnkZmkz9FCzLxEu0x+MdT0aVcODN/gY/aXejk0Uy8aZJ8XTkmxiagK/v3hiZ8UbXt/fHTi1DBB2xQIjnWcfBkyu0ml770aL84LDCsXhYxYvy/QrlcYNjCYFkm+RB71r2KmfBzajhmY1C+IfrkK+XnRB57aBL0TLkZ9Ygp0oi9saaV29tjqSAMzE2DdRrQe2Y4jTvVOMuzb/ZLHMgThQMDGv1+cgcU9Lix9FGmWghNgaJ2eglHoNBMDDml/4O1d8RuxJ+QJWEMVfGJVAvjIB086rYN02geiKwiKOvRr4+CGaj8d73eahTef9R31d0yTJrLLo+XQDrBHQj1lJKvro5BW8e910qp70oMCmtnDJDInc4ce51nHMFIyxDUJkJAHRScJBv11uwEUkNBcrAezdocpzHGPlpxBAxSLGLM2CB4/vxfMJmm/oLu++XgiWRRnAyG/RIdQQAJEOGWXGKo=";
+            string b=reqInfo.FromBase64Str();
+            string keyStr = "9EFE9DE8918E47E0B1718D2F0ABFjklt";
+            string key = keyStr.ToMd5();
+            string result = AES.AESDecrypt(reqInfo,key);
+            Assert.IsTrue(1==1);
         }
     }
 }

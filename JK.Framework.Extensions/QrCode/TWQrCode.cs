e984439641d64946e12331015b7b48dd43dc719d
jackie319 <jackie319@vip.qq.com>
2017-08-30 10:23:57 +0800

    带logo二维码

--- JK.Framework.Extensions/QrCode/TWQrCode.cs
+++ JK.Framework.Extensions/QrCode/TWQrCode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,8 @@ namespace JK.Framework.Extensions.QrCode
         /// <param name="content">内容</param>
         /// <param name="path">保存路径，不带“/”</param>
         /// <param name="picName">二维码名称</param>
-        public static void GenerateQrCode(string content,string path,string picName)
+        /// <param name="logoPath">logo路径（本地绝对路径）</param>
+        public static void GenerateQrCode(string content,string path,string picName,string logoPath)
         {
             QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
             qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
@@ -37,9 +39,66 @@ namespace JK.Framework.Extensions.QrCode
             string filepath = path +"/"+ filename;
             FileStream fs = new System.IO.FileStream(filepath, FileMode.OpenOrCreate, FileAccess.Write);
             image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+            //中间带logo
+            if (!string.IsNullOrEmpty(logoPath))
+            {
+                System.IO.MemoryStream MStream1 = new System.IO.MemoryStream();
+                CombinImage(image, logoPath).Save(MStream1, System.Drawing.Imaging.ImageFormat.Png);
+            }
+
             fs.Close();
             image.Dispose();
         }
+
+        /// <summary>
+        /// 调用此函数后使此两种图片合并，类似相册，有个
+        /// 背景图，中间贴自己的目标图片
+        /// </summary>
+        /// <param name="imgBack">粘贴的源图片</param>
+        /// <param name="destImg">粘贴的目标图片</param>
+        public static Image CombinImage(Image imgBack, string destImg)
+        {
+            Image img = Image.FromFile(destImg);    //照片图片
+            if (img.Height != 65 || img.Width != 65)
+            {
+                img = KiResizeImage(img, 65, 65, 0);
+            }
+            Graphics g = Graphics.FromImage(imgBack);
+            g.DrawImage(imgBack, 0, 0, imgBack.Width, imgBack.Height);   //g.DrawImage(imgBack, 0, 0, 相框宽, 相框高);
+                                                                         //g.FillRectangle(System.Drawing.Brushes.White, imgBack.Width / 2 - img.Width / 2 - 1, imgBack.Width / 2 - img.Width / 2 - 1,1,1);//相片四周刷一层黑色边框
+                                                                         //g.DrawImage(img, 照片与相框的左边距, 照片与相框的上边距, 照片宽, 照片高);
+            g.DrawImage(img, imgBack.Width / 2 - img.Width / 2, imgBack.Width / 2 - img.Width / 2, img.Width, img.Height);
+            GC.Collect();
+            return imgBack;
+        }
+
+        /// <summary>
+        /// Resize图片
+        /// </summary>
+        /// <param name="bmp">原始Bitmap</param>
+        /// <param name="newW">新的宽度</param>
+        /// <param name="newH">新的高度</param>
+        /// <param name="Mode">保留着，暂时未用</param>
+        /// <returns>处理以后的图片</returns>
+        public static Image KiResizeImage(Image bmp, int newW, int newH, int Mode)
+        {
+            try
+            {
+                Image b = new Bitmap(newW, newH);
+                Graphics g = Graphics.FromImage(b);
+                // 插值算法的质量
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(bmp, new Rectangle(0, 0, newW, newH), new Rectangle(0, 0, bmp.Width, bmp.Height), GraphicsUnit.Pixel);
+                g.Dispose();
+                return b;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+ 
+
 
 
         /// <summary>

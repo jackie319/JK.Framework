7fba61d355efcee6a3fa3800860091849526555b
jackie319 <jackie319@vip.qq.com>
2017-09-06 17:03:30 +0800

    InstancePerDependency

--- JK.Framework.Data/RegisterAutofacForJK.cs
+++ JK.Framework.Data/RegisterAutofacForJK.cs
@@ -32,10 +32,10 @@ namespace JK.Framework.Data
             ContainerBuilder builder = new ContainerBuilder();
             //builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces();
 
-            builder.Register<IDbContext>(c => new JKObjectContext(connectionStr)).InstancePerLifetimeScope();
+            builder.Register<IDbContext>(c => new JKObjectContext(connectionStr)).InstancePerDependency();
             //InstancePerDependency对每一个依赖或每一次调用创建一个新的唯一的实例
             //InstancePerLifetimeScope在一个生命周期域中，每一个依赖或调用创建一个单一的共享的实例，且每一个不同的生命周期域，实例是唯一的，不共享的。
-            builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerDependency();
             builder.RegisterType<MemoryCacheManager>().As<ICacheManager>().SingleInstance();
             registerAutofacDelegate(builder);
             //TODO:也可通过反射找到IDependencyRegistrar的实现类并调用方法
@@ -52,10 +52,10 @@ namespace JK.Framework.Data
 
             HttpConfiguration config = GlobalConfiguration.Configuration;
 
-            builder.Register<IDbContext>(c => new JKObjectContext(connectionStr)).InstancePerLifetimeScope();
+            builder.Register<IDbContext>(c => new JKObjectContext(connectionStr)).InstancePerDependency();
             //InstancePerDependency对每一个依赖或每一次调用创建一个新的唯一的实例
             //InstancePerLifetimeScope在一个生命周期域中，每一个依赖或调用创建一个单一的共享的实例，且每一个不同的生命周期域，实例是唯一的，不共享的。
-            builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(EfRepository<>)).As(typeof(IRepository<>)).InstancePerDependency();
             builder.RegisterType<MemoryCacheManager>().As<ICacheManager>().SingleInstance();
             registerAutofacDelegate(builder);
             //TODO:也可通过反射找到IDependencyRegistrar的实现类并调用方法

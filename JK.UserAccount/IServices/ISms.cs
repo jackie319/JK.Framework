1376da257a24fdfcf7d97c4536c65821ea08d188
jackie319 <jackie319@vip.qq.com>
2017-11-14 14:52:25 +0800

    sms

---
+++ JK.UserAccount/IServices/ISms.cs
@@ -0,0 +1,17 @@
+﻿using JK.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK.UserAccount.IServices
+{
+    public interface ISms
+    {
+        void SendCode(string phone, SmsTypeEnum type, string remark);
+        SmsRecords FindRecord(string phone, SmsTypeEnum smsType);
+        void Validate(Guid recordGuid);
+        void PayNotify(IList<string> moblieList, IList<string> paramList);
+    }
+}
--- JK.UserAccount/enum/SmsTypeEnum.cs
+++ JK.UserAccount/enum/SmsTypeEnum.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace JK.UserAccount
 {
-    public class Class1
+    public enum SmsTypeEnum
     {
+        Registe,
+        GetBackPwd,
+        Notify
     }
 }

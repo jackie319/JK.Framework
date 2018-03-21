ee754bd16689dcd0bed23ce5255bad4c4444e6b4
jackie319 <jackie319@vip.qq.com>
2018-03-21 14:14:46 +0800

    wxapplogin

---
+++ JK.CommonApi.WebApi/Controllers/WxAppLoginController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JK.CommonApi.WebApi.Controllers
+{
+    /// <summary>
+    /// 移动应用微信登录(App)
+    /// </summary>
+    public class WxAppLoginController : ApiController
+    {
+    }
+}
